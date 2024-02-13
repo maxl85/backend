f226f6dab0d06849fc51e6d7c590baae36fa5315
maxl85 <maxl85@mail.ru>
2024-02-13 13:55:42 +0300

    Added product

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getPostgresConfig } from './configs/postgres.config';
+import { PromoModule } from './promo/promo.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getPostgresConfig,
+    }),
+    PromoModule,
+    CategoryModule,
+    ProductModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
